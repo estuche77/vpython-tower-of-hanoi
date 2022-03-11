157e374ca5381b65d55bbc4c7970a1c9fd1bfc4f
Jason Latouche <96jaslat@gmail.com>
2022-03-10 21:18:39 -0600

    Updated code to be run with Python 3 and VPython/GlowScript

--- Modulo3D.py
+++ Modulo3D.py
@@ -4,60 +4,71 @@ Created on 10/6/2015
 @author: Estuche
 '''
 
-#Se importan librerias y demas modulos
-from visual import *
+# Se importan librerias y demas modulos
+from vpython import *
 import anillosMenu
 
 
 def mainwin():
-    #Se declaran las caracteristicas de la ventana
+    # Se declaran las caracteristicas de la ventana
     scene.x = 100
     scene.y = 100
     scene.height = 768
     scene.width = 1366
     scene.title = "Torres de Hanoi"
-    
-    #Se declaran caracteristicas de la ecena
+
+    # Se declaran caracteristicas de la ecena
     scene.background = color.gray(0.1)
-    scene.lights = [distant_light(direction = (0.22, 0.44, 0.88), color = color.gray(0.8))]
+    #scene.lights = [distant_light(direction = vector(0.22, 0.44, 0.88), color = color.gray(0.8))]
     #scene.userspin = False
     #scene.userzoom = False
-    scene.center = (0, 5, 0)
-    scene.forward = (0, -1, -3)
-    
-    #Se inicia las columnas
-    colIzq = cylinder(pos = (-12, 0, 0), axis = (0, 10, 0), radius = 0.4, color = (1.0,0.5,0), material = materials.wood)
-    colCen = cylinder(pos = (0, 0, 0), axis = (0, 10, 0), radius = 0.4, color = (1.0,0.5,0), material = materials.wood)
-    colDer = cylinder(pos = (12, 0, 0), axis = (0, 10, 0), radius = 0.4, color = (1.0,0.5,0), material = materials.wood)
-    
-    #Se inicia la base
-    baseTable = box(pos = (0, 0.15, 0), axis = (35, 0, 0), color = (1.0, 0.5, 0), width = 12, height = 0.5, material = materials.wood)
-    
-    #Se declaran variables globales
+    scene.center = vector(0, 5, 0)
+    scene.forward = vector(0, -1, -3)
+
+    # Se inicia las columnas
+    colIzq = cylinder(pos=vector(-12, 0, 0), axis=vector(0, 10, 0),
+                      radius=0.4, color=vector(1.0, 0.5, 0), texture=textures.stucco)
+    colCen = cylinder(pos=vector(0, 0, 0), axis=vector(
+        0, 10, 0), radius=0.4, color=vector(1.0, 0.5, 0), texture=textures.stucco)
+    colDer = cylinder(pos=vector(12, 0, 0), axis=vector(
+        0, 10, 0), radius=0.4, color=vector(1.0, 0.5, 0), texture=textures.stucco)
+
+    # Se inicia la base
+    baseTable = box(pos=vector(0, 0.15, 0), axis=vector(35, 0, 0), color=vector(
+        1.0, 0.5, 0), width=12, height=0.5, texture=textures.stucco)
+
+    # Se declaran variables globales
     palos = [colIzq, colCen, colDer]
     anillos = []
-    
-    #La lista de colore de anillos usada a la hora de crear anillos
-    colores = [(0.4, 0, 0), color.red, color.orange, color.yellow, color.green, color.cyan, color.blue, color.magenta, color.white]
-    
-    #Se llama la funcion que muestra un menu para elegir la cantidad de anillos a resolver
+
+    # La lista de colore de anillos usada a la hora de crear anillos
+    colores = [vector(0.4, 0, 0), color.red, color.orange, color.yellow,
+               color.green, color.cyan, color.blue, color.magenta, color.white]
+
+    # Se llama la funcion que muestra un menu para elegir la cantidad de anillos a resolver
     cantAnillos = anillosMenu.Anillos()
-    
-    #Se inicializan los anillos
+
+    # Se inicializan los anillos
     for i in range(9 - cantAnillos, 9):
-        anillos.append(ring (pos = (colIzq.x, len(anillos) + 1, 0), radius = 0.5 * (10 - i), thickness = 0.6, axis = (0, 1, 0), color = colores[i]))
-    
-    #Se apilan los anillos
+        anillos.append(ring(pos=vector(colIzq.pos.x, len(anillos) + 1, 0), radius=0.5 *
+                       (10 - i), thickness=0.6, axis=vector(0, 1, 0), color=colores[i]))
+
+    # Se apilan los anillos
     pila = [anillos, [], []]
-    
-    #Declara una flecha indicadora donde iran los anillos
-    arrow(pos = colDer.pos + (0, 14, 0), axis = (0, -1, 0), length = 2, shaftwidth = 0.4, headwidth = 0.8, color = color.green)
-    
-    #Declara texto para ser usado como constante de menu
-    autoTxt = text(text = "Auto Resolver", pos = (-6, 12, 0), align = "center", color = color.white, font = "sans")
-    userTxt = text(text = "Empezar", pos = (6, 12, 0), align = "center", color = color.white, font = "sans")
-    
-    #Se llama la funcion del submenu de los anillos
+
+    # Declara una flecha indicadora donde iran los anillos
+    arrow(pos=colDer.pos + vector(0, 14, 0), axis=vector(0, -1, 0), length=2,
+          shaftwidth=0.4, headwidth=0.8, color=color.green)
+
+    # Declara texto para ser usado como constante de menu
+    autoTxt = text(text="Auto Resolver", pos=vector(-6, 12, 0),
+                   align="center", color=color.white, font="sans")
+    userTxt = text(text="Empezar", pos=vector(6, 12, 0),
+                   align="center", color=color.white, font="sans")
+
+    # Se llama la funcion del submenu de los anillos
     anillosMenu.subMenu(baseTable, palos, pila, autoTxt, userTxt, cantAnillos)
 
-mainwin()
+
+if __name__ == "__main__":
+    mainwin()
--- anillosMenu.py
+++ anillosMenu.py
@@ -3,35 +3,42 @@ Created on 17/6/2015
 
 @author: Estuche
 '''
-from visual import *
+from vpython import *
 import Solver
 import userMoves
 
-#Fucion de menu de anillos
-def Anillos():
-    
-    #Se declaran las constantes de texto para mostrar como menu principal
-    tres = text(text = "3", pos = (-6, 12, 0), align = "center", color = color.white, font = "sans")
-    cuatro = text(text = "4", pos = (-4, 12, 0), align = "center", color = color.white, font = "sans")
-    cinco = text(text = "5", pos = (-2, 12, 0), align = "center", color = color.white, font = "sans")
-    seis = text(text = "6", pos = (0, 12, 0), align = "center", color = color.white, font = "sans")
-    siete = text(text = "7", pos = (2, 12, 0), align = "center", color = color.white, font = "sans")
-    ocho = text(text = "8", pos = (4, 12, 0), align = "center", color = color.white, font = "sans")
-    nueve = text(text = "9", pos = (6, 12, 0), align = "center", color = color.white, font = "sans")
+# Fucion de menu de anillos
 
-    #Se permite al usuario escoger la cantidad de anillos antes de iniciar el juego
+
+def Anillos():
+
+    # Se declaran las constantes de texto para mostrar como menu principal
+    tres = text(text="3", pos=vector(-6, 12, 0),
+                align="center", color=color.white, font="sans")
+    cuatro = text(text="4", pos=vector(-4, 12, 0),
+                  align="center", color=color.white, font="sans")
+    cinco = text(text="5", pos=vector(-2, 12, 0),
+                 align="center", color=color.white, font="sans")
+    seis = text(text="6", pos=vector(0, 12, 0),
+                align="center", color=color.white, font="sans")
+    siete = text(text="7", pos=vector(2, 12, 0),
+                 align="center", color=color.white, font="sans")
+    ocho = text(text="8", pos=vector(4, 12, 0),
+                align="center", color=color.white, font="sans")
+    nueve = text(text="9", pos=vector(6, 12, 0),
+                 align="center", color=color.white, font="sans")
+
+    # Se permite al usuario escoger la cantidad de anillos antes de iniciar el juego
     while True:
         rate(30)
-        if scene.mouse.clicked:
-            
-            #Se lee el click y se guarda en una variable su posicion
-            m = scene.mouse.getclick()
-            loc = m.pos            
-            
-            #La posicion del click se comparar con el area del texto por medio de 4 datos
-            if loc.x > tres.x - tres.width / 2 - 1 and loc.x < tres.x + tres.width / 2 + 1 and loc.y > tres.y - tres.height / 2 - 1 and loc.y < tres.y + tres.height / 2 + 1:
-                
-                #Esta seccion se encarga de hacer invisible y borrar todo el menu texto
+        ev = scene.waitfor('click')
+
+        if ev.event == 'click':
+
+            # La posicion del click se comparar con el area del texto por medio de 4 datos
+            if ev.pos.x > tres.pos.x - tres.width / 2 - 1 and ev.pos.x < tres.pos.x + tres.width / 2 + 1 and ev.pos.y > tres.pos.y - tres.height / 2 - 1 and ev.pos.y < tres.pos.y + tres.height / 2 + 1:
+
+                # Esta seccion se encarga de hacer invisible y borrar todo el menu texto
                 tres.visible = False
                 del tres
                 cuatro.visible = False
@@ -46,14 +53,14 @@ def Anillos():
                 del ocho
                 nueve.visible = False
                 del nueve
-                
-                #Retorna el numero presionado
+
+                # Retorna el numero presionado
                 return 3
-            
-            #La posicion del click se comparar con el area del texto por medio de 4 datos
-            elif loc.x > cuatro.x - cuatro.width / 2 - 1 and loc.x < cuatro.x + cuatro.width / 2 + 1 and loc.y > cuatro.y - cuatro.height / 2 - 1 and loc.y < cuatro.y + cuatro.height / 2 + 1:
-                
-                #Esta seccion se encarga de hacer invisible y borrar todo el menu texto
+
+            # La posicion del click se comparar con el area del texto por medio de 4 datos
+            elif ev.pos.x > cuatro.pos.x - cuatro.width / 2 - 1 and ev.pos.x < cuatro.pos.x + cuatro.width / 2 + 1 and ev.pos.y > cuatro.pos.y - cuatro.height / 2 - 1 and ev.pos.y < cuatro.pos.y + cuatro.height / 2 + 1:
+
+                # Esta seccion se encarga de hacer invisible y borrar todo el menu texto
                 tres.visible = False
                 del tres
                 cuatro.visible = False
@@ -68,14 +75,14 @@ def Anillos():
                 del ocho
                 nueve.visible = False
                 del nueve
-                
-                #Retorna el numero presionado
+
+                # Retorna el numero presionado
                 return 4
-            
-            #La posicion del click se comparar con el area del texto por medio de 4 datos
-            elif loc.x > cinco.x - cinco.width / 2 - 1 and loc.x < cinco.x + cinco.width / 2 + 1 and loc.y > cinco.y - cinco.height / 2 - 1 and loc.y < cinco.y + cinco.height / 2 + 1:
-                
-                #Esta seccion se encarga de hacer invisible y borrar todo el menu texto
+
+            # La posicion del click se comparar con el area del texto por medio de 4 datos
+            elif ev.pos.x > cinco.pos.x - cinco.width / 2 - 1 and ev.pos.x < cinco.pos.x + cinco.width / 2 + 1 and ev.pos.y > cinco.pos.y - cinco.height / 2 - 1 and ev.pos.y < cinco.pos.y + cinco.height / 2 + 1:
+
+                # Esta seccion se encarga de hacer invisible y borrar todo el menu texto
                 tres.visible = False
                 del tres
                 cuatro.visible = False
@@ -90,14 +97,14 @@ def Anillos():
                 del ocho
                 nueve.visible = False
                 del nueve
-                
-                #Retorna el numero presionado
+
+                # Retorna el numero presionado
                 return 5
-            
-            #La posicion del click se comparar con el area del texto por medio de 4 datos
-            elif loc.x > seis.x - seis.width / 2 - 1 and loc.x < seis.x + seis.width / 2 + 1 and loc.y > seis.y - seis.height / 2 - 1 and loc.y < seis.y + seis.height / 2 + 1:
-                
-                #Esta seccion se encarga de hacer invisible y borrar todo el menu texto
+
+            # La posicion del click se comparar con el area del texto por medio de 4 datos
+            elif ev.pos.x > seis.pos.x - seis.width / 2 - 1 and ev.pos.x < seis.pos.x + seis.width / 2 + 1 and ev.pos.y > seis.pos.y - seis.height / 2 - 1 and ev.pos.y < seis.pos.y + seis.height / 2 + 1:
+
+                # Esta seccion se encarga de hacer invisible y borrar todo el menu texto
                 tres.visible = False
                 del tres
                 cuatro.visible = False
@@ -112,14 +119,14 @@ def Anillos():
                 del ocho
                 nueve.visible = False
                 del nueve
-                
-                #Retorna el numero presionado
-                return 6              
-            
-            #La posicion del click se comparar con el area del texto por medio de 4 datos
-            elif loc.x > siete.x - siete.width / 2 - 1 and loc.x < siete.x + siete.width / 2 + 1 and loc.y > siete.y - siete.height / 2 - 1 and loc.y < siete.y + siete.height / 2 + 1:
-                
-                #Esta seccion se encarga de hacer invisible y borrar todo el menu texto
+
+                # Retorna el numero presionado
+                return 6
+
+            # La posicion del click se comparar con el area del texto por medio de 4 datos
+            elif ev.pos.x > siete.pos.x - siete.width / 2 - 1 and ev.pos.x < siete.pos.x + siete.width / 2 + 1 and ev.pos.y > siete.pos.y - siete.height / 2 - 1 and ev.pos.y < siete.pos.y + siete.height / 2 + 1:
+
+                # Esta seccion se encarga de hacer invisible y borrar todo el menu texto
                 tres.visible = False
                 del tres
                 cuatro.visible = False
@@ -134,14 +141,14 @@ def Anillos():
                 del ocho
                 nueve.visible = False
                 del nueve
-                
-                #Retorna el numero presionado
+
+                # Retorna el numero presionado
                 return 7
-            
-            #La posicion del click se comparar con el area del texto por medio de 4 datos
-            elif loc.x > ocho.x - ocho.width / 2 - 1 and loc.x < ocho.x + ocho.width / 2 + 1 and loc.y > ocho.y - ocho.height / 2 - 1 and loc.y < ocho.y + ocho.height / 2 + 1:
-                
-                #Esta seccion se encarga de hacer invisible y borrar todo el menu texto
+
+            # La posicion del click se comparar con el area del texto por medio de 4 datos
+            elif ev.pos.x > ocho.pos.x - ocho.width / 2 - 1 and ev.pos.x < ocho.pos.x + ocho.width / 2 + 1 and ev.pos.y > ocho.pos.y - ocho.height / 2 - 1 and ev.pos.y < ocho.pos.y + ocho.height / 2 + 1:
+
+                # Esta seccion se encarga de hacer invisible y borrar todo el menu texto
                 tres.visible = False
                 del tres
                 cuatro.visible = False
@@ -156,14 +163,14 @@ def Anillos():
                 del ocho
                 nueve.visible = False
                 del nueve
-                
-                #Retorna el numero presionado
+
+                # Retorna el numero presionado
                 return 8
-            
-            #La posicion del click se comparar con el area del texto por medio de 4 datos
-            elif loc.x > nueve.x - nueve.width / 2 - 1 and loc.x < nueve.x + nueve.width / 2 + 1 and loc.y > nueve.y - nueve.height / 2 - 1 and loc.y < nueve.y + nueve.height / 2 + 1:
-                
-                #Esta seccion se encarga de hacer invisible y borrar todo el menu texto
+
+            # La posicion del click se comparar con el area del texto por medio de 4 datos
+            elif ev.pos.x > nueve.pos.x - nueve.width / 2 - 1 and ev.pos.x < nueve.pos.x + nueve.width / 2 + 1 and ev.pos.y > nueve.pos.y - nueve.height / 2 - 1 and ev.pos.y < nueve.pos.y + nueve.height / 2 + 1:
+
+                # Esta seccion se encarga de hacer invisible y borrar todo el menu texto
                 tres.visible = False
                 del tres
                 cuatro.visible = False
@@ -178,50 +185,60 @@ def Anillos():
                 del ocho
                 nueve.visible = False
                 del nueve
-                
-                #Retorna el numero presionado
+
+                # Retorna el numero presionado
                 return 9
 
-#Fucion de submenu
+# Fucion de submenu
+
+
 def subMenu(baseTable, palos, pila, autoTxt, userTxt, cantAnillos):
-    
-    #Permite al usuario escoger entre dos menus
+
+    # Permite al usuario escoger entre dos menus
     while True:
-        rate(30)
-        
-        #Se lee el click y se guarda en una variable su posicion
-        if scene.mouse.clicked:
-            m = scene.mouse.getclick()
-            loc = m.pos
-            
-            #Texto de autoresolver, se compara su area en 4 variables
-            if loc.x > autoTxt.x - autoTxt.width / 2 and loc.x < autoTxt.x + autoTxt.width / 2 and loc.y > autoTxt.y - autoTxt.height / 2 and loc.y < autoTxt.y + autoTxt.height / 2:
-                
-                #Esta seccion se encarga de hacer invisible y borrar todo el menu texto
+        #rate(30)
+
+        # Se lee el click y se guarda en una variable su posicion
+        ev = scene.waitfor('click')
+
+        if ev.event == 'click':
+
+            # Texto de autoresolver, se compara su area en 4 variables
+            if ev.pos.x > autoTxt.pos.x - autoTxt.width / 2 and ev.pos.x < autoTxt.pos.x + autoTxt.width / 2 and ev.pos.y > autoTxt.pos.y - autoTxt.height / 2 and ev.pos.y < autoTxt.pos.y + autoTxt.height / 2:
+
+                # Esta seccion se encarga de hacer invisible y borrar todo el menu texto
                 userTxt.visible = False
                 del userTxt
                 autoTxt.visible = False
                 del autoTxt
-                text(text = str(2 ** cantAnillos - 1), pos = (-4, 12, 0), align = "center", color = color.white, font = "sans")
-                text(text = "movimientos", pos = (1, 12, 0), align = "center", color = color.white, font = "sans")
-                
-                #Se llama a la funcion perteneciente junto a sus parametros
+                text(text=str(2 ** cantAnillos - 1), pos=vector(-4, 12, 0),
+                     align="center", color=color.white, font="sans")
+                text(text="movimientos", pos=vector(1, 12, 0),
+                     align="center", color=color.white, font="sans")
+
+                # Se llama a la funcion perteneciente junto a sus parametros
                 Solver.hanoi(cantAnillos, 0, 1, 2, pila, palos)
-            
-            #Texto de reolver usario, se compara su area en 4 variables
-            elif loc.x > userTxt.x - userTxt.width / 2 and loc.x < userTxt.x + userTxt.width / 2 and loc.y > userTxt.y - userTxt.height / 2 and loc.y < userTxt.y + userTxt.height / 2:
-                
-                #Esta seccion se encarga de hacer invisible y borrar todo el menu texto
+                return
+
+            # Texto de reolver usario, se compara su area en 4 variables
+            elif ev.pos.x > userTxt.pos.x - userTxt.width / 2 and ev.pos.x < userTxt.pos.x + userTxt.width / 2 and ev.pos.y > userTxt.pos.y - userTxt.height / 2 and ev.pos.y < userTxt.pos.y + userTxt.height / 2:
+
+                # Esta seccion se encarga de hacer invisible y borrar todo el menu texto
                 userTxt.visible = False
                 del userTxt
                 autoTxt.visible = False
                 del autoTxt
-                
-                #Se declaran nuevos texto que se usaran en las siguientes ventanas
-                text(text = "Solucion Optima", pos = (-9, 12, 0), align = "center", color = color.white, font = "sans")
-                solu = text(text = str(2 ** cantAnillos - 1), pos = (-3, 12, 0), align = "center", color = color.white, font = "sans")
-                mov = text(text = str(0), pos = (2, 12, 0), align = "center", color = color.white, font = "sans")
-                text(text = "movimientos", pos = (7, 12, 0), align = "center", color = color.white, font = "sans")
-                
-                #Se llama a la funcion perteneciente junto a sus parametros
+
+                # Se declaran nuevos texto que se usaran en las siguientes ventanas
+                text(text="Solucion Optima", pos=vector(-9, 12, 0),
+                     align="center", color=color.white, font="sans")
+                solu = text(text=str(2 ** cantAnillos - 1), pos=vector(-3,
+                            12, 0), align="center", color=color.white, font="sans")
+                mov = text(text=str(0), pos=vector(2, 12, 0),
+                           align="center", color=color.white, font="sans")
+                text(text="movimientos", pos=vector(7, 12, 0),
+                     align="center", color=color.white, font="sans")
+
+                # Se llama a la funcion perteneciente junto a sus parametros
                 userMoves.user(baseTable, pila, palos, mov, solu)
+                return
